8298f63eb179c71be82888f60ce93d57800d0e11
Magnus Muru <magmur@ttu.ee>
2021-01-19 09:04:52 +0200

    Added CoordinateService for validated coordinate serving

--- src/main/java/ee/taltech/testing/courseproject/services/CoordinateService.java
+++ src/main/java/ee/taltech/testing/courseproject/services/CoordinateService.java
@@ -1,8 +1,11 @@
 package ee.taltech.testing.courseproject.services;
 
+import java.text.DecimalFormat;
+
 public class CoordinateService {
 
     public String getCoordinatesAsString(double inputLongitude, double inputLatitude) {
-        return "";
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        return decimalFormat.format(inputLatitude) + ", " + decimalFormat.format(inputLongitude);
     }
 }
