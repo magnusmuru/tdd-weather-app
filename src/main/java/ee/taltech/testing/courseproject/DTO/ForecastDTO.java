22035a54d55f2ebd792740d89fa61aea1885f3d3
Magnus Muru <magmur@ttu.ee>
2021-01-19 13:56:36 +0200

    Added necessary DTOs to get required forecast data

--- src/main/java/ee/taltech/testing/courseproject/DTO/ForecastDTO.java
+++ src/main/java/ee/taltech/testing/courseproject/DTO/ForecastDTO.java
@@ -1,6 +1,20 @@
 package ee.taltech.testing.courseproject.DTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ForecastDTO {
+    private int cod;
+    private Object message;
+    private List<ForecastItemDTO> forecasts;
 }
--- src/main/java/ee/taltech/testing/courseproject/DTO/ForecastItemDTO.java
+++ src/main/java/ee/taltech/testing/courseproject/DTO/ForecastItemDTO.java
@@ -1,4 +1,17 @@
 package ee.taltech.testing.courseproject.DTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ForecastItemDTO {
+    private DateDTO dt;
+    private WeatherDTO main;
 }
