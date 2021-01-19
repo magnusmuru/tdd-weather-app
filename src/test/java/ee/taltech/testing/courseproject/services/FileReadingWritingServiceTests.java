75ab4398dcbae83b3e4cf9ceb5cce27db7d1222e
Magnus Muru <magmur@ttu.ee>
2021-01-19 23:07:32 +0200

    Refacored file reader test in the case of running all tests

--- src/test/java/ee/taltech/testing/courseproject/services/FileReadingWritingServiceTests.java
+++ src/test/java/ee/taltech/testing/courseproject/services/FileReadingWritingServiceTests.java
@@ -5,6 +5,7 @@ import ee.taltech.testing.courseproject.exceptions.InvalidInputPathException;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,6 +18,12 @@ public class FileReadingWritingServiceTests {
 
     @Test
     public void fileReaderCanFindRightInputFileAndItsContents() throws InvalidInputPathException, IOException {
+        FileWriter writer;
+        Configuration configuration = new Configuration();
+        writer = new FileWriter(configuration.getWorkingFile());
+        writer.write("Tallinn");
+        writer.close();
+
         FileReadWriteService testService = new FileReadWriteService();
         List<String> expectedList = new ArrayList<>();
         expectedList.add("Tallinn");
