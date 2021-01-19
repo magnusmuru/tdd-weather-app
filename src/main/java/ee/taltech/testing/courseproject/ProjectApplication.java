0c160d312bd580caecc2413529dbf494706618cf
Magnus Muru <magmur@ttu.ee>
2021-01-19 21:12:35 +0200

    Added main application that uses input and makes output.

--- src/main/java/ee/taltech/testing/courseproject/ProjectApplication.java
+++ src/main/java/ee/taltech/testing/courseproject/ProjectApplication.java
@@ -1,13 +1,37 @@
 package ee.taltech.testing.courseproject;
 
+import ee.taltech.testing.courseproject.exceptions.InvalidInputPathException;
+import ee.taltech.testing.courseproject.services.FileReadWriteService;
+import ee.taltech.testing.courseproject.services.FileReportService;
+import lombok.AllArgsConstructor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
-public class ProjectApplication {
+@AllArgsConstructor
+public class ProjectApplication implements ApplicationRunner {
+
+    static Log log = LogFactory.getLog(ProjectApplication.class.getName());
+    private final FileReportService fileReportService;
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        try {
+            fileReportService.generateReports();
+            log.info("Successfully generated Weather reports for existing cities");
+        } catch (InvalidInputPathException | IOException e) {
+            log.error("No weather reports were generated");
+        }
     }
 
 }
