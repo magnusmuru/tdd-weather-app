b22578593cbcc65217aaa8d4c5f9218ca4d343da
Magnus Muru <magmur@ttu.ee>
2021-01-19 20:23:20 +0200

    Added verbose service additions since Tests were failing with annotated FileReadWriteService initialization.

--- src/test/java/ee/taltech/testing/courseproject/services/FileReadingWritingServiceTests.java
+++ src/test/java/ee/taltech/testing/courseproject/services/FileReadingWritingServiceTests.java
@@ -1,6 +1,5 @@
 package ee.taltech.testing.courseproject.services;
 
-import ee.taltech.testing.courseproject.API.WeatherAPI;
 import ee.taltech.testing.courseproject.configuration.Configuration;
 import ee.taltech.testing.courseproject.exceptions.InvalidInputPathException;
 import org.junit.Test;
@@ -13,34 +12,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileReadingWritingServiceTests {
 
-    private static FileReadWriteService fileReadWriteService;
-
-    @BeforeAll
-    public static void setUp() {
-        fileReadWriteService = new FileReadWriteService();
-    }
-
     @Test
     public void fileReaderCanFindRightInputFileAndItsContents() throws InvalidInputPathException, IOException {
+        FileReadWriteService testService = new FileReadWriteService();
         List<String> expectedList = new ArrayList<>();
         expectedList.add("Tallinn");
 
-        List<String> inputCities = fileReadWriteService.getInputFileContents();
+        List<String> inputCities = testService.getInputFileContents();
 
         assertEquals(expectedList, inputCities);
     }
 
     @Test
     public void fileWriterCanOutputReportContent() {
+        FileReadWriteService testService = new FileReadWriteService();
+
         String expectedCity = "Tallinn";
 
         Configuration configuration = new Configuration();
 
-        fileReadWriteService.writeWeatherReport(expectedCity);
+        testService.writeWeatherReport(expectedCity);
 
         Path path = Paths.get(configuration.getWorkingPath() + expectedCity.toLowerCase() + ".json");
 
