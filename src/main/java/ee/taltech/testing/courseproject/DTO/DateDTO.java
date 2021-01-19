71ced159024f62e1175cf32d64e645ec95be276f
Magnus Muru <magmur@ttu.ee>
2021-01-19 12:00:43 +0200

    Added further functionality for day format required in the original assignment

--- src/main/java/ee/taltech/testing/courseproject/DTO/DateDTO.java
+++ src/main/java/ee/taltech/testing/courseproject/DTO/DateDTO.java
@@ -1,16 +1,23 @@
 package ee.taltech.testing.courseproject.DTO;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class DateDTO {
-    private Date date;
+    private final Date date;
+
+    private static final long SECONDS_TO_MILLISECONDS = 1000;
+
+    public DateDTO(long date) {
+        this.date = new Date(date * SECONDS_TO_MILLISECONDS);
+    }
+
+    public String getFormattedDate() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(date);
+    }
 }
