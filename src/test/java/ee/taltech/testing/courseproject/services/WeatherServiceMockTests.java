2fa73f3670dcbf5dbc8bd5d8cd8cf7bebeb84f5b
Magnus Muru <magmur@ttu.ee>
2021-01-19 20:26:42 +0200

    Renamed WeatherServiceTests

--- src/test/java/ee/taltech/testing/courseproject/services/WeatherServiceMockTests.java
+++ src/test/java/ee/taltech/testing/courseproject/services/WeatherServiceMockTests.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class WeatherServiceMockTest {
+public class WeatherServiceMockTests {
 
     @Mock
     WeatherAPI weatherAPIMock;
