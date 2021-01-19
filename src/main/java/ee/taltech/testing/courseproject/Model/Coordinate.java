8bd5450731be2fce26f9c739d696c71de361a737
Magnus Muru <magmur@ttu.ee>
2021-01-19 16:55:55 +0200

    Added verbose decimal separator because it was defaulting to ',' even when system locale was set to '.'

--- src/main/java/ee/taltech/testing/courseproject/Model/Coordinate.java
+++ src/main/java/ee/taltech/testing/courseproject/Model/Coordinate.java
@@ -1,11 +1,17 @@
 package ee.taltech.testing.courseproject.Model;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 public class Coordinate {
 
     public String getCoordinatesAsString(double inputLongitude, double inputLatitude) {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setDecimalSeparator('.');
+
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        decimalFormat.setDecimalFormatSymbols(symbols);
+
         return decimalFormat.format(inputLatitude) + ", " + decimalFormat.format(inputLongitude);
     }
 }
