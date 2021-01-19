834a6c9bab9ece570088e82647a08cd98ef1f5c4
Magnus Muru <magmur@ttu.ee>
2021-01-19 10:59:23 +0200

    Added implementation to LocaleDetails that will serve coordinate and name locale info.

--- src/main/java/ee/taltech/testing/courseproject/API/WeatherAPI.java
+++ src/main/java/ee/taltech/testing/courseproject/API/WeatherAPI.java
@@ -6,14 +6,19 @@ import ee.taltech.testing.courseproject.Model.WeatherReportDetails;
 import ee.taltech.testing.courseproject.configuration.Configuration;
 import lombok.AllArgsConstructor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Objects;
 
 @Service
 public class WeatherAPI {
 
-    private final Configuration configuration = new Configuration();
-    private final ObjectMapper mapper = new ObjectMapper();
-    private final OkHttpClient client = new OkHttpClient();
+    private Configuration configuration = new Configuration();
+    private ObjectMapper mapper = new ObjectMapper();
+    private OkHttpClient client = new OkHttpClient();
 
     public WeatherReportDetails getCityDetails(String city) {
         return null;
@@ -23,7 +28,10 @@ public class WeatherAPI {
         return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + configuration.getApiKey();
     }
 
-    public CityDTO getCityLocaleDetails(String city) {
-        return null;
+    public CityDTO getCityLocaleDetails(String city) throws IOException {
+        Request request = new Request.Builder().url(currentWeatherUrl(city)).build();
+
+        Response response = client.newCall(request).execute();
+        return mapper.readValue(response.body().byteStream(), CityDTO.class);
     }
 }
